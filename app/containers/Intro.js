e7b101c2ef9eff4fb739168ec560c7f9f5c9dce5
Thomas Modeneis <thomas.modeneis@gmail.com>
2016-05-13 21:31:42 +0200

    #44

--- app/containers/Intro.js
+++ app/containers/Intro.js
@@ -12,8 +12,8 @@ import { loadSuggestCommentDetail } from 'actions/commentators'
 class Intro extends Component {
     static fetchData({ store, params }) {
         //let { index,value,skip,limit } = params;
-        var index = params.index || "languages";
-        var value = params.value || "english";
+        var index = params.index || "operator";
+        var value = params.value || "bbcuk";
         var skip = params.skip || "0";
         var limit = params.limit || "50";
 
